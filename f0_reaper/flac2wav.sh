831ca0005de71e1f546edda2ee3bf81f519862aa
psambit9791 <sambitpaul1992@gmail.com>
2021-06-11 09:48:03 +0100

    flac2wav and run_reaper auto detects files in a directory tree and operates on them. Outputs are in the same structure

--- f0_reaper/flac2wav.sh
+++ f0_reaper/flac2wav.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf data/audio/wavs/$1
+cp -r data/audio/flacs/$1 data/audio/wavs/
+find data/audio/wavs/$1 -iname "*.flac" | xargs rm
+
 for flacfile in `find data/audio/ -iname "*.flac"`
 do
     wavfile="${flacfile/flacs/wavs}"
--- f0_reaper/run_reaper.sh
+++ f0_reaper/run_reaper.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-export PATH="/home/sambit/Documents/Projects/slp_dissertation/f0_reaper/REAPER/build:$PATH"
-f0Folder="data/f0/"
-pmFolder="data/pm/"
-audioFolder="data/audio/"
-files=( $(ls $audioFolder | grep wav) )
+rm -rf data/f0/wavs/$1
+cp -r data/audio/wavs/$1 data/f0/wavs/
+find data/f0/wavs/ -iname "*.wav" | xargs rm
 
-for FILE in "${files[@]}";do
+rm -rf data/pm/wavs/$1
+cp -r data/audio/wavs/$1 data/pm/wavs/
+find data/pm/wavs/ -iname "*.wav" | xargs rm
+
+export PATH="/home/sambit/Documents/Projects/slp_dissertation/f0_reaper/REAPER/build:$PATH"
+audioFolder="data/audio/wavs/$1"
+
+for FILE in `find $audioFolder -iname "*.wav"`;
+do
 	name=$(echo "$FILE" | cut -f 1 -d '.')
-	reaper -i $audioFolder$FILE -f $f0Folder$name".f0" -p $pmFolder$name".pm" -a
+	reaper -i $FILE -f ${name/audio/f0}".f0" -p ${name/audio/pm}".pm" -a
 done
